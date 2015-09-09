733b1338c43fcc83fc92850d9cadbc1dbd38df06
Dan <daniel.keathley@gmail.com>
2015-09-08 20:51:41 -0400

    Initial commit

---
+++ src/main/java/com/merthin/reporting/tallyoh/App.java
@@ -0,0 +1,13 @@
+package com.merthin.reporting.tallyoh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
